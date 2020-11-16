7c51c8821371544c3518c185cc7ecf6ddb419478
Yu Tian Danny Chan <s10196363@connect.np.edu.sg>
2020-11-16 21:30:35 +0800

    Added Model for Room

---
+++ Realight_Website/DAL/RoomDAL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Realight_Website.Models;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Realight_Website.DAL
+{
+	public class RoomDAL
+	{
+	}
+}
